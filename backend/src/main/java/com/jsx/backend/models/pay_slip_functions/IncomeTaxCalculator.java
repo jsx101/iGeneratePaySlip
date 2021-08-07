7d02d22bbf4b29dc83eab883a3450a072e2bd951
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-07 18:15:37 +1000

    Added an IncomeTaxBracket inner class in IncomeTaxCalculator to make it more understandable

--- backend/src/main/java/com/jsx/backend/models/pay_slip_functions/IncomeTaxCalculator.java
+++ backend/src/main/java/com/jsx/backend/models/pay_slip_functions/IncomeTaxCalculator.java
@@ -7,25 +7,44 @@ import java.util.List;
 @Component
 public class IncomeTaxCalculator {
 
-    private List<Double> incomeUpperLimit = List.of(18200.0, 37000.0, 87000.0, 180000.0);
-    private List<Double> baseTax = List.of(0.0, 0.0, 3572.0, 19822.0, 54232.0);
-    private List<Double> amountPerDollar = List.of(0.0, 0.19, 0.325, 0.37, 0.45);
-    private List<Double> incomeLowerLimit = List.of(0.0, 18200.0, 37000.0, 87000.0, 180000.0);
+    private class IncomeTaxBracket {
+        private Double incomeLowerLimit;
+        private Double incomeUpperLimit;
+        private Double baseTax;
+        private Double taxPerDollar;
+        private Double whenOverThisMuch;
+
+        public IncomeTaxBracket(Double lower, Double upper, Double baseTax, Double taxPD, Double whenOver) {
+            this.incomeLowerLimit = lower;
+            this.incomeUpperLimit = upper;
+            this.baseTax = baseTax;
+            this.taxPerDollar = taxPD;
+            this.whenOverThisMuch = whenOver;
+        }
+    }
+
+    private List<IncomeTaxBracket> taxBrackets = List.of(
+            new IncomeTaxBracket(0.0,18200.0,0.0,0.0,0.0),
+            new IncomeTaxBracket(18201.0,37000.0,0.0,0.19,18200.0),
+            new IncomeTaxBracket(37001.0,87000.0,3572.0,0.325,37000.0),
+            new IncomeTaxBracket(87001.0,180000.0,19822.0,0.37,87000.0),
+            new IncomeTaxBracket(180000.0,Double.POSITIVE_INFINITY,54232.0,0.45,180000.0)
+    );
 
     public IncomeTaxCalculator() {
     }
 
     public Integer calculate(Integer annualSalary) {
-        if (annualSalary <= incomeUpperLimit.get(0)) {
+        if (annualSalary <= taxBrackets.get(0).incomeUpperLimit) {
             //System.out.println("Column 0");
             return calculateForTaxIncomeColumn(annualSalary, 0);
-        } else if (annualSalary <= incomeUpperLimit.get(1)) {
+        } else if (annualSalary <= taxBrackets.get(1).incomeUpperLimit) {
             //System.out.println("Column 1");
             return calculateForTaxIncomeColumn(annualSalary, 1);
-        } else if (annualSalary <= incomeUpperLimit.get(2)) {
+        } else if (annualSalary <= taxBrackets.get(2).incomeUpperLimit) {
             //System.out.println("Column 2");
             return calculateForTaxIncomeColumn(annualSalary, 2);
-        } else if (annualSalary <= incomeUpperLimit.get(3)) {
+        } else if (annualSalary <= taxBrackets.get(3).incomeUpperLimit) {
             //System.out.println("Column 3");
             return calculateForTaxIncomeColumn(annualSalary, 3);
         } else {
@@ -36,8 +55,8 @@ public class IncomeTaxCalculator {
 
     private Integer calculateForTaxIncomeColumn(Integer annualSalary, Integer index) {
 
-        Double amountOver = annualSalary - incomeLowerLimit.get(index);
+        Double amountOver = annualSalary - taxBrackets.get(index).whenOverThisMuch;
 
-        return (int) Math.round((baseTax.get(index) + amountOver*amountPerDollar.get(index))/12);
+        return (int) Math.round((taxBrackets.get(index).baseTax + amountOver*taxBrackets.get(index).taxPerDollar)/12);
     }
 }

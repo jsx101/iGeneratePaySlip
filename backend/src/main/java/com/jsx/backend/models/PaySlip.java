c4346239ad9b2615b0be48ca70972cd18391b601
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-07 18:13:40 +1000

    made the methods in PaySlip have "optional" parameters

--- backend/src/main/java/com/jsx/backend/models/PaySlip.java
+++ backend/src/main/java/com/jsx/backend/models/PaySlip.java
@@ -38,8 +38,18 @@ public class PaySlip {
         return this.grossIncome;
     }
 
+    public Integer returnGrossIncomeAmount(Integer annualSalary) {
+        this.grossIncome = grossIncomeCalculator.calculate(annualSalary);
+        return this.grossIncome;
+    }
+
     public Integer returnIncomeTaxAmount() {
         this.incomeTax = incomeTaxCalculator.calculate(employee.getAnnualSalary());
+        return incomeTax;
+    }
+
+    public Integer returnIncomeTaxAmount(Integer annualSalary) {
+        this.incomeTax = incomeTaxCalculator.calculate(annualSalary);
         return incomeTax;
     }
 
@@ -48,8 +58,18 @@ public class PaySlip {
         return this.netIncome;
     }
 
+    public Integer returnNetIncome(Integer annualSalary) {
+        this.netIncome = netIncomeCalculator.calculate(annualSalary);
+        return this.netIncome;
+    }
+
     public Integer returnSuperannuation() {
         this.superannuation = superannuationCalculator.calculate(employee.getAnnualSalary(), employee.getSuperRate());
+        return this.superannuation;
+    }
+
+    public Integer returnSuperannuation(Integer annualSalary, Double superRate) {
+        this.superannuation = superannuationCalculator.calculate(annualSalary, superRate);
         return this.superannuation;
     }
 
@@ -58,19 +78,20 @@ public class PaySlip {
         return this.paymentStartDate;
     }
 
+    public String returnPaymentStartDate(Integer paymentMonth) {
+        this.paymentStartDate = paymentPeriodIdentifier.getPaymentStartDate(paymentMonth);
+        return this.paymentStartDate;
+    }
+
     public String returnPaymentEndDate() {
         this.paymentEndDate = paymentPeriodIdentifier.getPaymentEndDate(employee.getPaymentMonth());
         return this.paymentEndDate;
     }
 
-    public EmployeeDetail getEmployee() {
-        return employee;
+    public String returnPaymentEndDate(Integer paymentMonth) {
+        this.paymentEndDate = paymentPeriodIdentifier.getPaymentEndDate(paymentMonth);
+        return this.paymentEndDate;
     }
-
-    public void setEmployee(EmployeeDetail employee) {
-        this.employee = employee;
-    }
-
 
     @Override
     public String toString() {
@@ -83,6 +104,42 @@ public class PaySlip {
                 ", \"paymentStartDate\":\"" + paymentStartDate + '"' +
                 ", \"paymentEndDate\":\"" + paymentEndDate + '"' +
                 '}';
+    }
+
+    // Getters and setters
+
+    public EmployeeDetail getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(EmployeeDetail employee) {
+        this.employee = employee;
+        this.returnGrossIncomeAmount();
+        this.returnIncomeTaxAmount();
+        this.returnNetIncome();
+        this.returnSuperannuation();
+        this.returnPaymentStartDate();
+        this.returnPaymentEndDate();
+    }
+
+    public Integer getIncomeTax() {
+        return incomeTax;
+    }
+
+    public Integer getNetIncome() {
+        return netIncome;
+    }
+
+    public Integer getSuperannuation() {
+        return superannuation;
+    }
+
+    public String getPaymentStartDate() {
+        return paymentStartDate;
+    }
+
+    public String getPaymentEndDate() {
+        return paymentEndDate;
     }
 }
 

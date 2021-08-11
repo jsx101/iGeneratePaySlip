ea8e18b812c3ed715a88eda8e40604ad49c2448b
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-11 19:01:41 +1000

    payslipcontroller now takes a list of json objects, have not fixed the test for this yet

--- backend/src/main/java/com/jsx/backend/controllers/PaySlipController.java
+++ backend/src/main/java/com/jsx/backend/controllers/PaySlipController.java
@@ -7,23 +7,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping(path="api")
 public class PaySlipController {
 
     // POST request with employee details
     @PostMapping(path="generate/pay-slip")
-    public String generatePaySlip(@RequestBody EmployeeDetail employee) {
+    public String generatePaySlip(@RequestBody List<EmployeeDetail> employees) {
 
-        PaySlip paySlip = new PaySlip(employee);
+        List<PaySlip> paySlips = new ArrayList<>();
 
-        paySlip.returnGrossIncomeAmount();
-        paySlip.returnIncomeTaxAmount();
-        paySlip.returnNetIncome();
-        paySlip.returnSuperannuation();
-        paySlip.returnPaymentStartDate();
-        paySlip.returnPaymentEndDate();
+        for(int i=0; i< employees.size(); i++){
+            paySlips.add(new PaySlip(employees.get(i)));
 
-        return paySlip.toString();
+            paySlips.get(i).returnGrossIncomeAmount();
+            paySlips.get(i).returnIncomeTaxAmount();
+            paySlips.get(i).returnNetIncome();
+            paySlips.get(i).returnSuperannuation();
+            paySlips.get(i).returnPaymentStartDate();
+            paySlips.get(i).returnPaymentEndDate();
+        }
+
+        return paySlips.toString();
     }
 }

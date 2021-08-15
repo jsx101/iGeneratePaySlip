19b2fb3368fde3e5fe69202f5e7f2192e9f774fe
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-16 01:23:41 +1000

    PaySlipControllerIntegration test doesn't work yet

--- backend/src/test/java/com/jsx/backend/controllers/PaySlipControllerIntegrationTest.java
+++ backend/src/test/java/com/jsx/backend/controllers/PaySlipControllerIntegrationTest.java
@@ -1,31 +1,84 @@
 package com.jsx.backend.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jsx.backend.businesslogic.PaySlipGenerator;
 import com.jsx.backend.models.EmployeeDetail;
+import com.jsx.backend.models.PaySlip;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.io.FilenameFilter;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest
+@WebMvcTest(controllers = PaySlipController.class)
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
 class PaySlipControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private PaySlipGenerator paySlipGenerator;
+
     // Tests if PaySlipController will take a valid request and return payslip data in the correct form
     @Test
     void generatePaySlip() throws Exception {
+        List<EmployeeDetail> employees = List.of(
+                new EmployeeDetail(
+                        "David",
+                        "Rudd",
+                        90500,
+                        0.09,
+                        1
+                ),
+                new EmployeeDetail(
+                        "Ryan",
+                        "Chen",
+                        120000,
+                        0.1,
+                        5
+                )
+        );
+
+        List<PaySlip> expectedOutput = List.of(
+                new PaySlip(
+                        employees.get(0),
+                        7542,
+                        1760,
+                        5782,
+                        679,
+                        "01 February",
+                        "28 February"
+                ),
+                new PaySlip(
+                        employees.get(1),
+                        10000,
+                        2669,
+                        7331,
+                        1000,
+                        "01 June",
+                        "30 June"
+                )
+        );
+
+        when(this.paySlipGenerator.returnPaySlips(employees)).thenReturn(expectedOutput);
+
         RequestBuilder request = MockMvcRequestBuilders
             .post("/api/pay-slip/generate")
             .content("[" +
@@ -47,7 +100,7 @@ class PaySlipControllerIntegrationTest {
             .contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON);
 
-        MvcResult result = mvc.perform(request).andReturn();
+        MvcResult result = this.mvc.perform(request).andReturn();
         assertEquals("[" +
             "{" +
                 "\"employee\":{" +

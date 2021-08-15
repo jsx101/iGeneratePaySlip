7c6a427efdae7f99f46aba5deae72efccf767ad7
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-16 00:22:24 +1000

    Removed @SpringBootTest. Not needed for PaymentPeriodIdentifierTest

--- backend/src/test/java/com/jsx/backend/businesslogic/calculators/PaymentPeriodIdentifierTest.java
+++ backend/src/test/java/com/jsx/backend/businesslogic/calculators/PaymentPeriodIdentifierTest.java
@@ -1,15 +1,10 @@
 package com.jsx.backend.businesslogic.calculators;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 class PaymentPeriodIdentifierTest {
 
     @Autowired

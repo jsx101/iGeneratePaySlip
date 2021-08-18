18b9dc2a693d1b0c3b81633b129f3b8512e1bf0c
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-18 14:31:38 +1000

    Added @SpringBootTest annotation to Gross Income Calculator and Payment Period Identifier test classes

--- backend/src/test/java/com/jsx/backend/businesslogic/calculators/GrossIncomeCalculatorTest.java
+++ backend/src/test/java/com/jsx/backend/businesslogic/calculators/GrossIncomeCalculatorTest.java
@@ -2,9 +2,14 @@ package com.jsx.backend.businesslogic.calculators;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @DisplayName("Gross income calculator")
 class GrossIncomeCalculatorTest {
     @Autowired
